d87b03d6c028f6fd2dd040b1c0860465d1fe6dd5
LearniuH <L13320500248@163.com>
2026-01-16 23:51:23 +0800

    自定义消息类型的话题实现

---
+++ zxz_ros_topic_ws/src/cpp01_topic/src/demo02_listener_stu.cpp
@@ -0,0 +1,45 @@
+/*
+需求：订阅学生信息，并在终端输出
+流程：
+    1.包含头文件
+    2.初始化 ROS2 客户端
+    3.自定义节点类
+        3-1.创建订阅方
+        3-2.解析并输出数据
+    4.调用spin函数，并传入节点对象指针
+    5.资源释放
+*/
+
+#include <rclcpp/rclcpp.hpp>
+#include <base_interfaces_demo/msg/student.hpp>
+
+class ListenerStu: public rclcpp::Node
+{
+public:
+    ListenerStu()
+    : Node("listener_stu")
+    {
+        // 3-1.创建订阅方
+        subscriber_ = this->create_subscription<base_interfaces_demo::msg::Student>("chatter_stu", 10, std::bind(&ListenerStu::do_cb, this, std::placeholders::_1));
+    }
+private:
+    void do_cb(const base_interfaces_demo::msg::Student &stu)
+    {
+        // 3-2.解析并输出数据
+        RCLCPP_INFO(this->get_logger(), "订阅到的学生信息：name=%s，age=%d，height=%.2f", stu.name.c_str(), stu.age, stu.height);
+    }
+    rclcpp::Subscription<base_interfaces_demo::msg::Student>::SharedPtr subscriber_;
+};
+
+int main(int argc, char * argv[])
+{
+    // 2.初始化 ROS2 客户端
+    rclcpp::init(argc, argv);
+
+    // 4.调用spin函数，并传入节点对象指针
+    rclcpp::spin(std::make_shared<ListenerStu>());
+
+    // 5.释放资源
+    rclcpp::shutdown();
+    return 0;
+}
--- zxz_ros_topic_ws/src/cpp01_topic/src/demo02_talker_stu.cpp
+++ zxz_ros_topic_ws/src/cpp01_topic/src/demo02_talker_stu.cpp
@@ -15,14 +15,33 @@
 #include <rclcpp/rclcpp.hpp>
 #include <base_interfaces_demo/msg/student.hpp>
 
+using namespace std::chrono_literals;
+
 class TalkerStu: public rclcpp::Node
 {
 public:
     TalkerStu()
     : Node("talker_stu")
     {
-
+        // 3-1.创建发布方
+        publisher_ = this->create_publisher<base_interfaces_demo::msg::Student>("chatter_stu", 10);
+        // 3-2.创建定时器
+        timer_ = this->create_wall_timer(1s, bind(&TalkerStu::on_timer, this));
     }
+private:
+    void on_timer()
+    {
+        // 3-3.组织并发布学生消息
+        auto stu = base_interfaces_demo::msg::Student();
+        stu.name = "LearniuH";
+        stu.age = 24;
+        stu.height = 1.72;
+
+        publisher_->publish(stu);
+        RCLCPP_INFO(this->get_logger(), "发布的消息：%s, %d, %.2f", stu.name.c_str(), stu.age, stu.height);
+    }
+    rclcpp::Publisher<base_interfaces_demo::msg::Student>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char * argv[])
