5e8a2e4efa0d76b0be55ebafa83da73535adc100
LearniuH <L13320500248@163.com>
2026-01-21 14:15:16 +0800

    创建全局(相对ROS2根节点，与命名空间同级)/相对（相对命名空间，与节点名称同级）/私有话题（相对节点名称，是节点的子集）

--- zxz_ros_topic_ws/src/cpp05_names/src/demo01_names.cpp
+++ zxz_ros_topic_ws/src/cpp05_names/src/demo01_names.cpp
@@ -1,18 +1,29 @@
-/*
-通过编码方式实现设置节点名称/命名空间, 解决节点重名
-*/
-
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
 
 class NodeCPP: public rclcpp::Node
 {
 public:
     NodeCPP()
-    : Node("cpp_node", "ns_cpp")
+    /*
+    通过编码方式实现设置节点名称/命名空间, 解决节点重名
+    */
+    : Node("node_name", "namespace")
     // Node(const rclcpp::Node &other, const std::string &sub_namespace)
     {
-        
+        // 全局话题：/global_topic
+        global_pub_ = this->create_publisher<std_msgs::msg::String>("/global_topic", 10);
+
+        // 相对话题：/namespace/relative_topic
+        relative_pub_ = this->create_publisher<std_msgs::msg::String>("relative_topic", 10);
+
+        // 私有话题：/namespace/node_name/private_topic
+        private_pub_ = this->create_publisher<std_msgs::msg::String>("~/private_topic", 10);
     }
+private:
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr global_pub_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr relative_pub_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr private_pub_;
 };
 
 int main(int argc, char * argv[])
